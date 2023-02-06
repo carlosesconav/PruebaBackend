a5f509a680c481313b31217cd1d1d2d1f36b0c29
Carlos A. Escobar <carlosesconav@gmail.com>
2023-02-06 18:27:20 -0500

    Add register and Login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use \stdClass;
 
 class AuthController extends Controller
 {
-    //
+
+  public function register(Request $request)
+  {
+    $validator = Validator::make($request->all(), 
+     [
+       'name' => 'required|string|max:255',
+       'email' => 'required|string|email|max:255|unique:users',
+       'password' => 'required|string|min:8'
+     ]);
+
+    if($validator->fails())
+    {
+      return response()->json($validator->erros());
+    }
+    
+    $user = User::create(
+    [
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password)
+    ]);
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json(['data' => $user,'access_token' => $token, 'token_type' => 'Bearer',]);
+  }
+
+  public function login(Request $request)
+  {
+    if(!Auth::attempt($request->only('email','password')))
+    {
+      $message = ['message' => 'Unauthorized'];
+      return response()->json($message, 401);
+    }
+
+    $user = User::where('email', $request['email'])->firstOrFail();
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    $message = 
+    [
+      'message' => 'WELCOME USER',
+      'accessToken' => $token,
+      'token_type' => 'Bearer',
+      'user' => $user
+    ];
+
+    return response()->json($message);
+  }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
-    
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FavoritesController;
+use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::get('favorites',[FavoritesController::class,'index']);
+Route::post('register', [AuthController::class,'register']);
+Route::post('login', [AuthController::class,'login']);
