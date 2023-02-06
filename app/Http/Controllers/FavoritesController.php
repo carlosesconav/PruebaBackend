a8b8fd8ffb43375835bfa1c9791a73b0438263fd
Carlos A. Escobar <carlosesconav@gmail.com>
2023-02-06 16:20:16 -0500

    first commit

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Favorites;
+use Illuminate\Http\Request;
+
+class FavoritesController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Favorites $favorites)
+    {
+        //
+    }
+
+    public function edit(Favorites $favorites)
+    {
+        //
+    }
+
+    public function update(Request $request, Favorites $favorites)
+    {
+        //
+    }
+
+    public function destroy(Favorites $favorites)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_02_06_210038_create_favorites_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->bigInteger('id_user');
+            $table->string('ref_api');
+            
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
